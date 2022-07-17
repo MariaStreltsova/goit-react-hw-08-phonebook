d2efc4f7b3c71dbead3a4628e8122e51f2b866dc
MariaStreltsova <Mariastreltova@gmail.com>
2022-07-17 20:06:33 +0300

    adds initialContacts

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,18 +6,15 @@ import { Filter } from './filter/Filter';
 import { Container, Title } from './App.styled';
 
 export default function App() {
+  const initialContacts = [
+    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+  ];
   const [contacts, setContacts] = useState(() => {
-    const persistedContacts = JSON.parse(localStorage.getItem('contacts'));
-    return persistedContacts
-      ? persistedContacts
-      : [
-          { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-          { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-          { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-          { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-        ];
+    return JSON.parse(localStorage.getItem('contacts')) ?? initialContacts;
   });
-
   const [filter, setFilter] = useState('');
 
   useEffect(() => {
